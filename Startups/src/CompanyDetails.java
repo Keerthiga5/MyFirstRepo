aec70f17088426c4ab856024f85c753b7141a8d6
User <User@DESKTOP-D43KOLD>
2018-07-17 15:37:54 +0530

    rectified CompanyDetails

--- Startups/src/CompanyDetails.java
+++ Startups/src/CompanyDetails.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 public class CompanyDetails {
 	
-	private static String URL="jdbc:h2:~/STARTUP",className="org.h2.Driver",sql;
+	private static String URL="jdbc:h2:~/STARTUP",className="org.h2.Driver";
 	private static Connection con;
 	private static Statement stmt;
 	private static ResultSet rs;
@@ -17,8 +17,8 @@ public class CompanyDetails {
 	private static String ins_founder_query="INSERT INTO FOUNDERS VALUES(default,?,?,?,?,?)";
 	
 	
-	public int insert(String c_name,String city,String email,String linkedin,String facebook,String site,String blog,
-			String fname,int cid,String f_lnkdin,String twitter,String f_fblink) throws SQLException
+	public int insertCompany(String c_name,String city,String email,String linkedin,
+			String facebook,String site,String blog) throws SQLException
 	{
 		ins_comp=con.prepareStatement(ins_comp_query);
 		ins_comp.setString(1,c_name);
@@ -28,20 +28,18 @@ public class CompanyDetails {
 		ins_comp.setString(5,facebook);
 		ins_comp.setString(6,site);
 		ins_comp.setString(7,blog);
-		int x1= ins_comp.executeUpdate();
-		
+		return ins_comp.executeUpdate();
+	}
+	
+	public int insertFounder(String fname,int cid,String f_lnkdin,String twitter,String f_fblink)throws SQLException
+	{	
 		ins_founder=con.prepareStatement(ins_founder_query);
 		ins_founder.setString(1,fname);
 		ins_founder.setInt(2,cid);
 		ins_founder.setString(3,f_lnkdin);
 		ins_founder.setString(4,twitter);
 		ins_founder.setString(5,f_fblink);
-		int x2=ins_founder.executeUpdate();
-		
-		if(x1>=0 && x2>=0)
-			return 1;
-		else 
-			return 0;
+		 return ins_founder.executeUpdate();		
 	}
 	
 	public void create() throws SQLException
@@ -58,7 +56,7 @@ public class CompanyDetails {
 		
 	}
 	
-	public void retrieve() throws SQLException
+	public void printCompany() throws SQLException
 	{
 		rs=stmt.executeQuery("select * from COMPANY");
 		System.out.println("COMPANY table:");
@@ -67,7 +65,11 @@ public class CompanyDetails {
 			System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3)+"\t"+rs.getString(4)
 			+"\t"+rs.getString(5)+"\t"+rs.getString(6)+"\t"+rs.getString(7)+"\t"+rs.getString(8));
 		}
-		
+		rs.close();
+	}
+	
+	public void printFounder()throws SQLException
+	{
 		rs=stmt.executeQuery("select * from FOUNDERS");
 		System.out.println("FOUNDERS table");
 		while(rs.next())
@@ -77,7 +79,6 @@ public class CompanyDetails {
 		}
 		
 		rs.close();
-		
 	}
 
 	public static void main(String[] args) {
@@ -94,31 +95,46 @@ public class CompanyDetails {
 			cd.create();			
 			
 			//inserting company records
-			n+=cd.insert("9stacks","New Delhi","care@9stacks.com","https://www.linkedin.com/company/9stacks/","https://facebook.com/9stacksGaming",
-					"https://www.9stacks.com","https://www.9stacks.in/","Sudhir Kamath",1,"https://www.linkedin.com/in/kamathsudhir",
-					"https://twitter.com/sudhirkamath","https://www.facebook.com/kamathsudhir");
+			n+=cd.insertCompany("9stacks","New Delhi","care@9stacks.com","https://www.linkedin.com/company/9stacks/","https://facebook.com/9stacksGaming",
+					"https://www.9stacks.com","https://www.9stacks.in/");
 			
-			n+=cd.insert("doodleblue innovations","Chennai","genius@doodleblue.com","https://www.linkedin.com/company/doodleblue","htttps://www.facebook.com/",
-					"https://www.doodleblue.com","https://www.doodleblue.com/blog.php","Atishe Chordia",2,"https://www.linkedin.com/in/atishe-chordia-567a8723",
+			n+=cd.insertCompany("doodleblue innovations","Chennai","genius@doodleblue.com","https://www.linkedin.com/company/doodleblue","htttps://www.facebook.com/",
+					"https://www.doodleblue.com","https://www.doodleblue.com/blog.php");
+			
+			n+=cd.insertCompany("Karomi Smart Solutions","Chennai","support@karomi.com","https://www.linkedin.com/company/karomi-technology",
+					"https://www.facebook.com/karomi","http://www.karomi.com","http://karomi.com/blog/");
+			
+			n+=cd.insertCompany("Firstcry","Pune","customercare@firstcry.com","https://in.linkedin.com/company/firstcry","https://facebook.com/FirstCryIndia",
+					"http://www.firstcry.com","https://blog.firstcry.com");
+			
+			n+=cd.insertCompany("Callhealth","Hyderabad","companysecretary@callhealth.com","https://www.linkedin.com/company/call-health",
+					"https://www.facebook.com/callhealthofficial","http://callhealth.com","https://blog.callhealth.com");
+			
+			System.out.println(n+" records inserted in Company table");
+			n=0;
+			
+			n+=cd.insertFounder("Sudhir Kamath",1,"https://www.linkedin.com/in/kamathsudhir","https://twitter.com/sudhirkamath",
+					"https://www.facebook.com/kamathsudhir");
+			
+			n+=cd.insertFounder("Atishe Chordia",2,"https://www.linkedin.com/in/atishe-chordia-567a8723",
 					"https://www.twitter.com/atishechordia","https://www.facebook.com/atishe");
-			
-			n+=cd.insert("Karomi Smart Solutions","Chennai","support@karomi.com","https://www.linkedin.com/company/karomi-technology",
-					"https://www.facebook.com/karomi","http://www.karomi.com","http://karomi.com/blog/","Vilva Natarajan",3,
-					"https://www.linkedin.com/in/vilva-natarajan-805917","https://www.twitter.com/vivlva_karomi","https://www.facebook.com/vilva.natarajan");
-			
-			n+=cd.insert("Firstcry","Pune","customercare@firstcry.com","https://in.linkedin.com/company/firstcry","https://facebook.com/FirstCryIndia",
-					"http://www.firstcry.com","https://blog.firstcry.com","Amitava Saha",4,"https://www.linkedin.com/in/amitavasaha73",
+					
+			n+=cd.insertFounder("Vilva Natarajan",3,"https://www.linkedin.com/in/vilva-natarajan-805917",
+					"https://www.twitter.com/vivlva_karomi","https://www.facebook.com/vilva.natarajan");
+					
+			n+=cd.insertFounder("Amitava Saha",4,"https://www.linkedin.com/in/amitavasaha73",
 					"NULL","https://www.facebook.com/amitava.saha.39");
+					
+			n+=cd.insertFounder("Hari Thalapalli",5,"https://www.linkedin.com/in/hari-thalapalli-1bb45112",
+					"https://twitter.com/h_thalapalli?lang=en","https://www.facebook.com/hari.thalapalli.5");
 			
-			n+=cd.insert("Callhealth","Hyderabad","companysecretary@callhealth.com","https://www.linkedin.com/company/call-health",
-					"https://www.facebook.com/callhealthofficial","http://callhealth.com","https://blog.callhealth.com","Hari Thalapalli",5,
-					"https://www.linkedin.com/in/hari-thalapalli-1bb45112","https://twitter.com/h_thalapalli?lang=en",
-					"https://www.facebook.com/hari.thalapalli.5");
-			
-			System.out.println(n+" records inserted");
+			System.out.println(n+"records inserted in Founders table");
 			
 			//retrieving data
-			cd.retrieve();
+			cd.printCompany();
+			
+			cd.printFounder();
+			
 			stmt.close();
 			con.close();
 		}
